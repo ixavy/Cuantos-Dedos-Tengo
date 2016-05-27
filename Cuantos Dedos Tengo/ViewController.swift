abd83901fadd4c63b9c20104ad81cbae8fd09c36
Javier <development@ixavy.com>
2016-05-27 14:12:34 +0200

    commit automatico a github

--- Cuantos Dedos Tengo/ViewController.swift
+++ Cuantos Dedos Tengo/ViewController.swift
@@ -8,8 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+    
+    var arrayNumber = [UInt32]()
+    
+    let maxNumber:UInt32 = 10
+
+    @IBOutlet weak var inputFingers: UITextField!
+    
+    @IBOutlet weak var outputText: UILabel!
+    
+    @IBAction func buttonPressed(sender: AnyObject) {
+        
+        let randomFingers = arc4random_uniform(maxNumber)
+        
+        if inputFingers.text == "" {
+            outputText.text = "Introduce un número del 0 al 9"
+        } else {
+            if let numberInput = inputFingers.text {
+                if UInt32(numberInput) == randomFingers {
+                    outputText.text = "ACERTASTE !!!. El número era \(randomFingers)"
+                } else {
+                    outputText.text = "FALLASTE !!!. El número era \(randomFingers)"
+                }
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
